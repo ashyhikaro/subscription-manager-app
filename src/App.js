43440b77d0f2ddba9c3de13c3ee4c51b3e998d44
Oleh Marynchuk <73314452+ashyhikaro@users.noreply.github.com>
2023-04-11 16:40:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ import { SidePanel } from './components/sidepanelComponents/SidePanel';
 
 function App() {
   const [sum, setSum] = useState(localStorage.getItem('sumOfSubs') ? localStorage.getItem('sumOfSubs') : 0.00)
-
-  // useEffect(() => {
-  //   let pricesArr = document.querySelectorAll('.sub-price')
-  //   let sumOfPrices = 0
-  //   pricesArr.forEach(item => {
-  //     sumOfPrices += parseFloat((item.innerText.split(' ')[1]))
-  //   })
-  //   setSum(sumOfPrices.toFixed(2))
-  // })
-
-  // useEffect(() => {localStorage.setItem('sumOfSubs', sum)}, [sum])
-
+  
   return (
     <div className='app-window'>
       <MainPanel setSum={setSum} sum={sum}/>
